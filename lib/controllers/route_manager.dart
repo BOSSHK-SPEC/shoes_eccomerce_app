c8f8e4972c39a8cb35afed6248e11b1fcb3b0da1
Yashwanth H K <yashwanthhk2000@gmail.com>
2023-05-16 10:24:09 +0530

    updated commit

---
+++ lib/controllers/route_manager.dart
@@ -0,0 +1,31 @@
+import 'package:shoe_stores/views/carts/carts.dart';
+import 'package:shoe_stores/views/orders/orders.dart';
+import '../views/main_screen.dart';
+import '../views/orders/payment_acknowledgement.dart';
+import '../views/product/details.dart';
+import '../views/splash/entry.dart';
+import '../views/splash/splash.dart';
+import '../views/store/details.dart';
+
+class RouteManager {
+  static const String splashScreen = "/splash";
+  static const String entryScreen = "/entry_screen";
+  static const String mainScreen = '/customer_home_screen';
+  static const String productDetails = '/product_details_screen';
+  static const String storeScreen = '/store_screen';
+  static const String cartScreen = '/cart_screen';
+  static const String orderScreen = '/order_screen';
+  static const String paymentAcknowledgement = '/payment_acknowledgement';
+}
+
+final routes = {
+  RouteManager.splashScreen: (context) => const SplashScreen(),
+  RouteManager.entryScreen: (context) => const EntryScreen(),
+  RouteManager.mainScreen: (context) => const MainScreen(),
+  RouteManager.productDetails: (context) => const ProductDetails(),
+  RouteManager.storeScreen: (context) => const StoreDetails(),
+  RouteManager.cartScreen: (context) => const CartScreen(),
+  RouteManager.orderScreen: (context) => const OrdersScreen(),
+  RouteManager.paymentAcknowledgement: (context) =>
+      const PaymentAcknowledgementScreen(),
+};
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:shoe_stores/providers/cart.dart';
+import 'package:shoe_stores/providers/category.dart';
+import 'package:shoe_stores/providers/orders.dart';
+import 'package:shoe_stores/providers/product.dart';
+import 'package:shoe_stores/providers/reviews.dart';
+import 'package:shoe_stores/providers/stores.dart';
+import 'package:shoe_stores/resources/theme_manager.dart';
+import 'package:shoe_stores/views/splash/entry.dart';
+import 'constants/color.dart';
+import 'controllers/route_manager.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          systemNavigationBarColor: accentColor,
+          statusBarBrightness: Brightness.dark),
+    );
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => Products()),
+        ChangeNotifierProvider(create: (context) => Cart()),
+        ChangeNotifierProvider(create: (context) => Reviews()),
+        ChangeNotifierProvider(create: (context) => Stores()),
+        ChangeNotifierProvider(create: (context) => Categories()),
+        ChangeNotifierProvider(create: (context) => Orders()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: getLightTheme(),
+        title: 'Shoe\'s Store',
+        home: const EntryScreen(),
+        routes: routes,
+
+      ),
+    );
+  }
+}
